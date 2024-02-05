c777605f0e595938a72cd01adc1be077ffa36f3d
Naresh Deeti <nareshdeetind@outlook.com>
2024-02-05 19:04:54 +0530

    Add nonplussed-login

---
+++ nonplussed-login/src/main/java/com/deeti/nonplussed/NonplussedLoginApplication.java
@@ -0,0 +1,13 @@
+package com.deeti.nonplussed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonplussedLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonplussedLoginApplication.class, args);
+	}
+
+}
---
+++ nonplussed-login/src/test/java/com/deeti/nonplussed/NonplussedLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeti.nonplussed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonplussedLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
