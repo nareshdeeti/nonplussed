126a03bb41a73777d2471ac3c5707ae0aacf7cfe
Naresh Deeti <nareshdeeti@hotmail.com>
2024-07-16 21:58:31 +0530

    initial commit for nonplussed-simple-jwt

---
+++ nonplussed-simple-jwt/README.md
@@ -0,0 +1,3 @@
+# nonplussed-simple-jwt
+
+Simple `JWT` authorization application using [Spring Authorization](https://docs.spring.io/spring-authorization-server/reference/overview.html) and [Resource](https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/index.html) servers
---
+++ nonplussed-simple-jwt/src/main/java/com/deeti/nonplussed/NonplussedSimpleJwtApplication.java
@@ -0,0 +1,13 @@
+package com.deeti.nonplussed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonplussedSimpleJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonplussedSimpleJwtApplication.class, args);
+    }
+
+}
