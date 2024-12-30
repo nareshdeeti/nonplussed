a49ca0b4e20a9af94ce0474846f3fb203652ec47
Naresh Deeti <nareshdeetind@outlook.com>
2024-12-30 18:31:31 +0530

    Adds nonplussed-oauth-jpa example

---
+++ nonplussed-oauth/nonplussed-oauth-jpa/src/main/java/com/deeti/nonplussed/AuthorizationConsentRepository.java
@@ -0,0 +1,14 @@
+package com.deeti.nonplussed;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorizationConsentRepository extends JpaRepository<AuthorizationConsent, AuthorizationConsent.AuthorizationConsentId> {
+
+    Optional<AuthorizationConsent> findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+}
---
+++ nonplussed-oauth/nonplussed-oauth-jpa/src/main/java/com/deeti/nonplussed/NonplussedOauthJpaApplication.java
@@ -0,0 +1,13 @@
+package com.deeti.nonplussed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonplussedOauthJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonplussedOauthJpaApplication.class, args);
+	}
+
+}
